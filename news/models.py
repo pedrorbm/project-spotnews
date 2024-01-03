c34dc8d58e0bc8a8f23aa733e90f410f09beb46e
Pedro Medeiros <pedropwks@gmail.com>
2024-01-02 21:34:27 -0300

    Feat: Adicionando a migrate e a model News

--- news/models.py
+++ news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from news.verifications import title_verify
 
 
 class Category(models.Model):
@@ -16,3 +17,17 @@ class User(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class News(models.Model):
+    title = models.CharField(
+        max_length=200, blank=False, validators=[title_verify]
+    )
+    content = models.TextField(blank=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateField(blank=False)
+    image = models.ImageField(upload_to="img/", blank=True, null=True)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.title
